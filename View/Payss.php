33c574517298aad186cfcf40daf07a74396c1075
laamiri-kaoutar <kaoutarlaamiri355@gmail.com>
2024-12-26 15:04:53 +0100

    Create the delete function that allow the user to delete a city

--- View/Payss.php
+++ View/Payss.php
@@ -1,29 +1,5 @@
 <?php 
 
-// i do not now what is that for but i will leave it for now 
-// include 'connexion.php';
-
-//     $data = "SELECT p.nom as nomPays, langues, population ,urlImage , c.nom as nomContinent,p.id_pays from pays p,continent c where p.id_continent = c.id_continent ";
-//     $continet = "SELECT * FROM continent.continent";
-
-//     $continentName= $conn->query($continet);
-//     $result = $conn->query($data);
-
-//     if (isset($_GET['id'])) {
-//         $id=$_GET['id'];
-//         $sql="SELECT p.nom as nomPays, langues, population ,urlImage , c.nom as nomContinent,p.id_pays from pays p,continent c where p.id_continent = c.id_continent and id_pays=$id ";
-//         $q = $conn->query($sql);
-//         $ligne= $q->fetch_assoc();
-//         $nomPays = $ligne['nomPays'];
-//         $population = $ligne['population'];
-//         $langues = $ligne['langues'];
-//         $nomContinent = $ligne['nomContinent'];
-//         $urlImage = $ligne['urlImage'];
-//     }
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
-
-// here i get all the pays we have to display them 
-
 require_once '../Controller/PaysController.php';
 
 $paysController = new PaysController();
